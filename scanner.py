1331862d6656144bc3153e5ccee3dcf7c2aedcff
Reshad Ahmed <reshad4587@gmail.com>
2023-10-30 15:19:54 -0400

    Implemented threading for faster results

--- scanner.py
+++ scanner.py
@@ -1,38 +1,42 @@
 import socket
-import sys
+import argparse
 import pyfiglet
+import concurrent.futures
 from datetime import datetime
 
-ascii_banner = pyfiglet.figlet_format("PORT SCANNER")
-print(ascii_banner)
+def scan_port(target, port):
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socket.setdefaulttimeout(1)
+        result = sock.connect_ex((target, port))
+        if result == 0:
+            print(f"Port {port} is open")
+        sock.close()
+    except (socket.timeout, ConnectionRefusedError, OSError):
+        pass
 
-if len(sys.argv) == 2:
-    target = socket.gethostbyname(sys.argv[1])
+def scan_ports(target):
     print("-" * 50)
     print(f"Scanning target: {target}")
     print(f"Started scanning at: {str(datetime.now())}")
     print("-" * 50)
 
-    try:
-        for port in range(1, 65535):
-            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            socket.setdefaulttimeout(1)
+    with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
+        futures = [executor.submit(scan_port, target, port) for port in range(1, 65535)]
 
-            result = sock.connect_ex((target, port))
-            if result == 0:
-                print(f"Port {port} is open")
-            sock.close()
-    except KeyboardInterrupt:
-        print("\nKeyboard interrupt! Exiting program.")
-        sys.exit()
-    except socket.gaierror:
-        print("\nHostname could not be resolved. Exiting program.")
-        sys.exit()
-    except socket.error:
-        print("\nNo response from server. Exiting program.")
-        sys.exit()
-else:
-    print("Usage: python portscanner.py <target_hostname>")
+        for future in concurrent.futures.as_completed(futures):
+            pass
 
-            
-            
+def main():
+    parser = argparse.ArgumentParser(description="Python Port Scanner")
+    parser.add_argument("target", help="The target hostname or IP address to scan")
+    args = parser.parse_args()
+
+    ascii_banner = pyfiglet.figlet_format("PORT SCANNER")
+    print(ascii_banner)
+
+    target = socket.gethostbyname(args.target)
+    scan_ports(target)
+
+if __name__ == "__main__":
+    main()
